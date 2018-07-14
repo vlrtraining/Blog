8b671230e57068178c31b70e4f4f86d75f5e4c7e
rameshvch <ramesh.vch@gmail.com>
2018-07-14 18:55:48 +0530

    jobs app added to front end

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
