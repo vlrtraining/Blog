ba09799707e379406a1f5b6df3754ed1e0641619
rameshvch <ramesh.vch@gmail.com>
2018-07-15 13:20:26 +0530

    post app with summary and time

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,9 @@ class Posts(models.Model):
     body = models.TextField()
     pub_date=models.DateTimeField()
     image = models.ImageField(upload_to='images/')
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:200]
+    def sdate(self):
+        return self.pub_date.strftime(' %b %e %Y')
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
+def allposts(request):
+    posts=Posts.objects
+    return render(request,'posts/allposts.html',{'posts':posts})
