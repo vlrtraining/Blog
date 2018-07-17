2c2fe571093426a05232c9ee7402cd54e05ca2d8
rameshvch <ramesh.vch@gmail.com>
2018-07-17 10:41:12 +0530

    finl blog project from vlrtraining

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
-
     path('', views.allposts,name='allposts'),
-
+    path('<int:posts_id>/',views.detail,name='detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Posts
 
 # Create your views here.
 def allposts(request):
     posts=Posts.objects
     return render(request,'posts/allposts.html',{'posts':posts})
+def detail(request,posts_id):
+    po=get_object_or_404(Posts,pk=posts_id)
+    return render(request,'posts/detail.html',{'post':po})
